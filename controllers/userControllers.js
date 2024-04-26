b8dd7b2ad0bc360b6ac08b6862f3793b64dc59ba
siddhu_from_sklm <codewithsiddhu@gmail.com>
2024-04-26 15:50:39 +0530

    Create tailwind Properties

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -194,13 +194,14 @@ export const registerUser = async (req, res) => {
 
 export const fetchUsers = async (req, res) => {
   try {
-    const users = await User.find({}, { sub: 0, idUpload: 0, refreals: 0, regEvents: 0, regWorkshop: 0 }); // Excluding specified fields
+    const users = await User.find({}, '-sub -idUpload -refreals -regEvents -regWorkshop'); // Excluding specified fields
     return res.status(200).json({ users });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
 
 
 export const fetchUserById = async (req, res) => {
